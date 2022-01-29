fcbb67ffffbbd668e47b705280fae58bf7b65388
Ari Seyhun <ariseyhun@live.com.au>
2022-01-29 19:09:25 +0700

    better data structure for wasm

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -7,29 +7,40 @@ import!("./domain.wit");
 
 pub use domain::{Domain, DomainData};
 
+struct Data {
+    //wasi structure here
+    wasi: wasmtime_wasi::WasiCtx,
+    // and any extra data for wasm here, including domain data:
+    domain: DomainData,
+}
+
 fn main() -> Result<()> {
     // Define the WASI functions globally on the `Config`.
     let engine = Engine::default();
-    let mut linker = Linker::new(&engine);
-    wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
+    let mut linker = Linker::<Data>::new(&engine);
+    wasmtime_wasi::add_to_linker(&mut linker, |ctx| &mut ctx.wasi)?;
 
     // Create a WASI context and put it in a Store; all instances in the store
     // share this context. `WasiCtxBuilder` provides a number of ways to
     // configure what the target program will have access to.
-    let mut wasi = WasiCtxBuilder::new()
+    let wasi = WasiCtxBuilder::new()
         .inherit_stdio()
         .inherit_args()?
         .build();
-    let domain_index = wasi.table().push(Box::new(DomainData::default()))?;
-    let mut store = Store::new(&engine, wasi);
+    let mut store = Store::new(
+        &engine,
+        Data {
+            wasi,
+            domain: DomainData::default(),
+        },
+    );
 
     // Instantiate our module with the imports we've created, and run it.
     let module = Module::from_file(&engine, "target/wasm32-wasi/release/domain.wasm")?;
     // linker.module(&mut store, "", &module)?;
 
-    let (domain, instance) = Domain::instantiate(&mut store, &module, &mut linker, move |s| {
-        s.table().get_mut::<DomainData>(domain_index).unwrap()
-    })?;
+    let (domain, _instance) =
+        Domain::instantiate(&mut store, &module, &mut linker, move |ctx| &mut ctx.domain)?;
 
     let s = domain.add(&mut store, "hey")?;
     println!("{}", s);
